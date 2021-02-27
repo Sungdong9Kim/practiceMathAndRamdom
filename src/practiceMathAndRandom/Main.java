06a3589c3cc235dc1183a6cb23eb693c33139996
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-02-27 23:01:32 +0900

    feat : init practicing math and random code in java

---
+++ src/practiceMathAndRandom/Main.java
@@ -0,0 +1,17 @@
+package practiceMathAndRandom;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+		//Math.abs : 절대값을 구하는 함수(function for evaluating absolute value)
+		System.out.println(Math.abs(-10));
+		System.out.println(Math.abs(8));
+		
+		//Math.min(x, y) : x와 y중 더 작은 수를 리턴하는 함수(function for returning the smaller value of x and y)
+		//Math.max(x, y) : x와 y중 더 큰 수를 리턴하는 함수(function for returning the greater value of x and y)
+		System.out.println(Math.min(4, 10));
+		System.out.println(Math.max(4, 10));
+		
+	}
+}
